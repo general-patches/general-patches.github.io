323cb13fabe25314bf807b436319b34980d5aa8c
general-patches <62354158+general-patches@users.noreply.github.com>
2020-03-19 10:33:16 +0200

    Second commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var data = null;
-var filled = false;
+const canvasWidth = 1000;
+const canvasHeight = 700;
+const canvasPadding = 50;
 
 /*
 ctx.save();
@@ -19,10 +21,9 @@ function changeCountry(event){
     if(event.keyCode == 13){
         load(true);
         if(document.getElementById("countryCode").value != ""){
-            if(filled == true){
-                for(var i = 0; i < 1; i++){
-                    removeElement("canvas_container" + i);
-                } 
+            if(document.getElementById("canvas_container0") != null){
+                removeElement("canvas_container0");
+                removeElement("container_heading0");
             }
             update(document.getElementById("countryCode").value);
             document.getElementById("countryCode").value = "";
@@ -31,27 +32,29 @@ function changeCountry(event){
         }
     }
 }
-
-var iconLoad = setInterval(rotate,10);
 var deg = 0;
+var iconLoad;
+
+async function load(bool){
+    var icon = document.getElementById("icon");
+    if(bool == true){
+        iconLoad = setInterval(rotate,10);
+        icon.style.border = "10px solid black";
+        icon.style.height = "50px";
+        icon.style.borderTopColor = "white";
+    }else{
+        icon.style.border = "0px solid black";
+        icon.style.height = "0px";
+        clearInterval(iconLoad);
+    }
+}
+
 function rotate(){
     if(deg > 359){
         deg = 0;
     }
     document.getElementById("icon").style.transform = "rotate("+deg+"deg)";
     deg += 3;
-}
-
-function load(bool){
-    var icon = document.getElementById("icon");
-    if(bool == true){
-        icon.style.border = "10px solid black";
-        icon.style.height = "50px";
-        icon.style.borderTopColor = "white";    
-    }else{
-        icon.style.border = "0px solid black";
-        icon.style.height = "0px";
-    }
 }
 
 async function update(countryCode = "ZA"){
@@ -62,17 +65,22 @@ async function update(countryCode = "ZA"){
     await promise;
     
     load(false);
-    for(var i = 0; i < 1; i++){
-        filled = true;
-        var app = document.getElementById("body_container");
-        var canvasContainer = document.createElement("div");
-        canvasContainer.setAttribute("class","canvas_container");
-        canvasContainer.setAttribute("id","canvas_container" + i);
-        app.appendChild(canvasContainer);
-        canvasContainer.innerHTML = '<canvas id="canvas' + i + '" style="border: 2px solid black;background-color: white;border-radius: 4px;padding: 10px;" width="1020" height="720">';
-    } 
+    insertDataArea("0");
+    drawBarGraph("canvas0");
+}
 
-    drawBarGraph("canvas" + 0);
+function insertDataArea(name){
+    var app = document.getElementById("body_container");
+    var div = document.createElement("div");
+    div.setAttribute("class","container_heading");
+    div.setAttribute("id","container_heading" + name);
+    app.appendChild(div);
+    div.innerHTML = "<h1 style='padding-bottom:10px;'>Cumulative cases</h1>";
+    var canvasContainer = document.createElement("div");
+    canvasContainer.setAttribute("class","canvas_container");
+    canvasContainer.setAttribute("id","canvas_container" + name);
+    app.appendChild(canvasContainer);
+    canvasContainer.innerHTML = '<canvas id="canvas' + name + '" style="border: 2px solid black;background-color: white;border-radius: 4px;" width="'+canvasWidth+'" height="'+canvasHeight+'">';
 }
 
 function drawBarGraph(canvasID){
@@ -90,17 +98,26 @@ function drawBarGraph(canvasID){
         }
     }
     
-    x_adjust = 1020/x_values[(Object.keys(data).length-3)];
-    y_adjust = 720/maxY;
+    x_adjust = (canvasWidth-2*canvasPadding)/x_values[(Object.keys(data).length-3)];
+    y_adjust = (canvasHeight-2*canvasPadding)/maxY;
     
     ctx.save();
-    ctx.moveTo(0, 720);
+    ctx.moveTo(canvasPadding, canvasHeight-canvasPadding);
     ctx.lineWidth = 3;
     for(var i = 0; i < (Object.keys(data).length-2); i++){
         y_values[i] = y_values[i]*y_adjust;
-        ctx.lineTo(x_values[i]*x_adjust,(720-y_values[i]));
+        x_values[i] = x_values[i]*x_adjust;
+        ctx.lineTo(x_values[i]+canvasPadding,(canvasHeight-y_values[i])-canvasPadding);
         //(1-(y_values[i]/maxY))*maxY
     }
+    ctx.stroke();
+    ctx.save();
+    ctx.font = "20px sans-serif";
+    ctx.fillText("Time",canvasWidth/2,canvasHeight-canvasPadding/2.5);
+    ctx.fillText(maxY,canvasPadding/2,canvasPadding+8);
+    ctx.lineWidth = 1;
+    ctx.moveTo(canvasPadding/2+60,canvasPadding);
+    ctx.lineTo(canvasWidth-canvasPadding,canvasPadding);
     ctx.stroke();
     ctx.restore();
 }
